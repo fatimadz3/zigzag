00a8a8ec720b92a87625f581e4f6a9dfbcf86074
fatimadz3 <141044105+fatimadz3@users.noreply.github.com>
2023-07-31 07:40:25 -0700

    Update index.js
    update

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x22e525c895eff8f8bc2236c1bac3905b354b2095";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
